accfd37f617376404acd23bf2bcd1c20dd4a4f03
Ing. Gabriel Solano <dgabrielsolo@gmail.com>
2024-02-01 17:57:19 -0600

    .

--- pdf_from_excel.py
+++ pdf_from_excel.py
@@ -6,8 +6,11 @@ def xlpdf(file_location):
     app.Interactive = False
     app.Visible = False
     wb = app.Workbooks.open(file_location)
-    output = r'C:\Users\dgabr\OneDrive\Documentos\Gabriel (cloud)\DeskPyLab\Lab - Projects for sale\Log - DeskPyLab.pdf'
+    output = r'C:\Users\dgabr\OneDrive\Documentos\Gabriel (cloud)\DeskPyLab\Lab - Projects for sale\XL-PDF drawer\Stylesheet - DeskPyLab.pdf'
     wb.ActiveSheet.ExportAsFixedFormat(0, output)
     wb.Close()
 
-xlpdf(r'C:\Users\dgabr\OneDrive\Documentos\Gabriel (cloud)\DeskPyLab\Lab - Projects for sale\Log.xlsx')
+    try: os.system('taskkill /f /im excel.exe')
+    except Exception as e: print(e)
+
+xlpdf(r'C:\Users\dgabr\OneDrive\Documentos\Gabriel (cloud)\DeskPyLab\Lab - Projects for sale\XL-PDF drawer\Stylesheet.xlsx')
